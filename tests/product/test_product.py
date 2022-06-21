06a006a6aea648f6b3c1f9ef9ed857b023dc25b0
johntvale <johntvale@gmail.com>
2022-06-21 18:44:46 -0400

    Requisito 1

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        12345001,
+        'notebook',
+        'dell',
+        '01/01/2022',
+        '01/01/2030',
+        123456789,
+        'instrucoes de armazenamento',
+        )
+
+    assert product.id == 12345001
+    assert product.nome_do_produto == 'notebook'
+    assert product.nome_da_empresa == 'dell'
+    assert product.data_de_fabricacao == '01/01/2022'
+    assert product.data_de_validade == '01/01/2030'
+    assert product.numero_de_serie == 123456789
+    assert product.instrucoes_de_armazenamento == 'instrucoes de armazenamento'
