d74a31a6e0cfd62521d8e5615f3350c625fa9d64
johntvale <johntvale@gmail.com>
2022-06-21 19:40:09 -0400

    Requisito 2

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,24 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        12345002,
+        'Mouse',
+        'logitech',
+        '01/01/2020',
+        '01/01/2050',
+        987654321,
+        'em um lugar seguro',
+        )
+
+    result = product.__repr__()
+
+    assert type(result) == str
+    assert result == (
+        "O produto Mouse "
+        "fabricado em 01/01/2020 "
+        "por logitech com validade "
+        "até 01/01/2050 "
+        "precisa ser armazenado em um lugar seguro."
+    )
