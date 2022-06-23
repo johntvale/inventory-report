26d6d3333cc3da0a51baf37d869a90e4a30cf010
johntvale <johntvale@gmail.com>
2022-06-22 21:47:58 -0400

    Requisito 7

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import xmltodict
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -20,12 +21,21 @@ class Inventory():
             file_json = json.load(file)
             return file_json
 
+    def read_xml(path):
+        with open(path, encoding="utf-8") as file:
+            file_xml = file.read()
+            file_read = xmltodict.parse(file_xml)['dataset']
+            print(file_read)
+            return file_read['record']
+
     def import_data(path, type):
         data = []
         if path.endswith('.csv'):
             data = Inventory.read_csv(path)
         if path.endswith('.json'):
             data = Inventory.read_json(path)
+        if path.endswith('.xml'):
+            data = Inventory.read_xml(path)
 
         if type == 'simples':
             return SimpleReport.generate(data)
